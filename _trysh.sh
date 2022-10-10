e4b0fe1e8168041024566a549003963b86fc7c76
openroot <dev.openroot@gmail.com>
2022-10-11 03:08:56 +0530

    temp progress

--- _trysh.sh
+++ _trysh.sh
@@ -1,34 +1,78 @@
+#!/bin/bash
+# Name: _trysh.sh
+# Purpose: trying bash script
+# ---------------------------
+
+# region welcome
+
 echo "_trysh"
-echo "+strange easy world"
+echo "༂࿐~ +strange easy world"
 echo
 
-## testing area
-# a=5.0
-# b=2
-# op=$(( $a % $b ))
-# echo "$op"
+# region end
 
-## input area
+# region function
+
+function math () {
+	local value1=$1
+	local operation=$2
+	local value2=$3
+	local scale=$4
+	local output=$(bc -l <<< "scale=$scale; $value1 $operation $value2")
+	echo "$output"
+}
+
+# region end
+
+
+# region testing
+
+	# attested start
+	# value1=100.12
+	# value2=3
+	# scale=3
+	# total=$(math $(math $value1 '*' $value2 $scale) '/' 20 3)
+	# echo "sample math operation :: ($value1 * $value2) / 20 = $total"
+	# attested end
+
+	# echo
+
+# region end
+
+# region input
+
 read -rp "put quote: " "quote"
 read -rp "put time (in seconds): " "time"
 read -rp "put loop count: " "loopcount"
 echo
 
-## execute area
+# region end
+
+# region execute
+
 a=0
 while [ $a -lt $loopcount ]
 do
-	a=`expr $a + 1`
+	a=$(math $a '+' 1 0)
 
-	echo " ༂࿐~ $quote: heard [ step $a. ]"
+	echo " ༂࿐~~$quote+:heard [step $a] [time elapsed $(math $a '*' $time 5) sec(s)] "
 
 	sleep $time
 done
 
-## report area
+# region end
 
-## exit area
+# region report
+
+
+
+# region end
+
+# region exit
+
 echo
 echo ",say enter any string to exit: "
 read dump
 echo "$dump"
+
+# region end
